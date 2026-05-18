6362c0067dc004603b2431cec4e291fb4018841a
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-18 19:19:20 +0200

    feat: tst-2 wip: refactoring

--- snack.test.js
+++ snack.test.js
@@ -15,3 +15,22 @@ const { getInitials } = require("./test-vari.js")
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
     expect(getInitials("Marco")).toBe("Ma")
 })
+
+
+
+// 🏆 Snack 2
+// Creare un test che verifichi la seguente descrizione:
+
+// 👉 "La funzione createSlug restituisce una stringa in lowercase."
+
+function createSlug(str) {
+    return str.toLowerCase()
+}
+
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+    expect(createSlug("Marco Rossi")).toBe("marco rossi")
+        expect(createSlug("Loris MAGICO")).toBe("loris magico")
+
+})
+
+
