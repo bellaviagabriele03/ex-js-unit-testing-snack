750b9325a59de7b64aaa260efb05f858cd6d6de0
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-18 19:12:05 +0200

    FEAT: snack 1 done WIP: refactoring

--- snack.test.js
+++ snack.test.js
@@ -2,7 +2,10 @@
 // Creare un test che verifichi la seguente descrizione:
 
 // 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
-    
+function getInitials(name) {
+    const nameArray = name.split("");
+    return nameArray[0] + nameArray[1]
+}
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
     expect(getInitials("Marco")).toBe("Ma")
