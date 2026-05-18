987c94928b53b45040550415627307cdbe5c5cdd
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-18 19:35:53 +0200

    feat: test-3

--- snack.test.js
+++ snack.test.js
@@ -1,4 +1,4 @@
-const { getInitials } = require("./test-vari.js")
+const { getInitials, createSlug } = require("./test-vari.js")
 
 
 
@@ -23,14 +23,31 @@ test("La funzione getInitials restituisce le iniziali di un nome completo.", () 
 
 // 👉 "La funzione createSlug restituisce una stringa in lowercase."
 
-function createSlug(str) {
-    return str.toLowerCase()
-}
 
 test("La funzione createSlug restituisce una stringa in lowercase.", () => {
     expect(createSlug("Marco Rossi")).toBe("marco rossi")
-        expect(createSlug("Loris MAGICO")).toBe("loris magico")
+    expect(createSlug("Loris MAGICO")).toBe("loris magico")
 
 })
 
 
+
+// 🏆 Snack 3
+// Creare un test che verifichi la seguente descrizione:
+
+// 👉 "La funzione average calcola la media aritmetica di un array di numeri."
+
+function average(nums) {
+    const somma = nums.reduce((acc, n)=> acc + n, 0)
+    return somma / nums.length
+}
+
+
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+
+    const numbers = [1, 2, 3, 4,]
+    const numbers2 = [30, 40, 50, 100]
+
+    expect(average(numbers)).toBe(2.5)
+    expect(average(numbers2)).toBe(55)
+})
--- test-vari.js
+++ test-vari.js
@@ -2,8 +2,12 @@ function getInitials(name) {
     const nameArray = name.split("");
     return nameArray[0] + nameArray[1]
 }
+function createSlug(str) {
+    return str.toLowerCase()
+}
 
 
 module.exports = {
     getInitials,
+    createSlug,
 }
